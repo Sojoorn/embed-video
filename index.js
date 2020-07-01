f2601e329f00d2c6efd2b3f6d1701158d1e446dc
Hugo Pérez <hugoa.perezh@outlook.com>
2020-07-01 16:28:01 -0500

    JS Validation

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-var URL = require('url')
-var URI = require('uri-js')
-var promise = require('promise-polyfill')
-var fetch = require('fetch-ponyfill')({ Promise: promise }).fetch
-var escape = require('lodash.escape')
+const URL = require('url');
+const URI = require('uri-js');
+const promise = require('promise-polyfill');
+const {fetch} = require('fetch-ponyfill')({Promise: promise});
+const escape = require('lodash.escape');
 
-var YOUTUBE = 'youtube'
-var VIMEO = 'vimeo'
-var DAILYMOTION = 'dailymotion'
+const YOUTUBE = 'youtube';
+const VIMEO = 'vimeo';
+const DAILYMOTION = 'dailymotion';
 
-var validVimeoOpts = [
+const validVimeoOpts = [
   'thumbnail_small',
   'thumbnail_medium',
-  'thumbnail_large'
-]
-var validYouTubeOpts = [
+  'thumbnail_large',
+];
+const validYouTubeOpts = [
   'default',
   'mqdefault',
   'hqdefault',
   'sddefault',
-  'maxresdefault'
-]
-var validDailyMotionOpts = [
+  'maxresdefault',
+];
+const validDailyMotionOpts = [
   'thumbnail_60_url',
   'thumbnail_120_url',
   'thumbnail_180_url',
@@ -28,232 +28,234 @@ var validDailyMotionOpts = [
   'thumbnail_360_url',
   'thumbnail_480_url',
   'thumbnail_720_url',
-  'thumbnail_1080_url'
-]
+  'thumbnail_1080_url',
+];
 
-var VIMEO_MATCH_RE = /^(?:\/video|\/channels\/[\w-]+|\/groups\/[\w-]+\/videos)?\/(\d+)/
+const VIMEO_MATCH_RE = /^(?:\/video|\/channels\/[\w-]+|\/groups\/[\w-]+\/videos)?\/(\d+)/;
 
-function embed (url, opts) {
-  var res = embed.info(url)
-  return res && embed[res.source] && embed[res.source](res.id, opts)
+function embed(url, opts) {
+  const res = embed.info(url);
+  return res && embed[res.source] && embed[res.source](res.id, opts);
 }
 
-embed.info = function (url) {
-  var uri = URI.parse(url)
-  url = URL.parse(url, true)
+embed.info = function(url) {
+  const uri = URI.parse(url);
+  url = URL.parse(url, true);
 
-  var id
+  let id;
 
-  id = detectYoutube(url, uri)
+  id = detectYoutube(url, uri);
   if (id) {
     return {
-      id: id,
+      id,
       source: YOUTUBE,
       url: url.href,
-      embedUrl: '//www.youtube.com/embed/' + id,
-    }
+      embedUrl: `//www.youtube.com/embed/${id}`,
+    };
   }
 
-  id = detectVimeo(url, uri)
+  id = detectVimeo(url, uri);
   if (id) {
     return {
-      id: id,
+      id,
       source: VIMEO,
       url: url.href,
-      embedUrl: '//player.vimeo.com/video/' + id,
-    }
+      embedUrl: `//player.vimeo.com/video/${id}`,
+    };
   }
 
-  id = detectDailymotion(url, uri)
+  id = detectDailymotion(url, uri);
   if (id) {
     return {
-      id: id,
+      id,
       source: DAILYMOTION,
       url: url.href,
-      embedUrl: '//www.dailymotion.com/embed/video/' + id,
-    }
+      embedUrl: `//www.dailymotion.com/embed/video/${id}`,
+    };
   }
-}
+};
 
 // For compat with <=2.0.1
-embed.videoSource = embed.info
+embed.videoSource = embed.info;
 
-embed.image = function (url, opts, cb) {
+embed.image = function(url, opts, cb) {
   if (typeof opts === 'function') {
-    cb = opts
-    opts = {}
+    cb = opts;
+    opts = {};
   }
-  opts = opts || {}
+  opts = opts || {};
 
-  var res = embed.info(url)
-  if (!res && cb) return setTimeout(function () { cb() })
-  return res && embed[res.source].image(res.id, opts, cb)
+  const res = embed.info(url);
+  if (!res && cb) {
+    return setTimeout(() => {
+      cb();
+    });
+  }
+  return res && embed[res.source].image(res.id, opts, cb);
+};
+
+function detectVimeo(url, uri) {
+  let match;
+  return (uri.host === 'vimeo.com' && (match = VIMEO_MATCH_RE.exec(url.pathname))) ? match[1] : null;
 }
 
-function detectVimeo (url, uri) {
-  var match
-  return (uri.host === 'vimeo.com' && (match = VIMEO_MATCH_RE.exec(url.pathname))) ? match[1] : null
-}
-
-function detectYoutube (url, uri) {
+function detectYoutube(url, uri) {
   if (uri.host.indexOf('youtube.com') > -1) {
-    return url.query.v
+    return url.query.v;
   }
 
   if (uri.host === 'youtu.be') {
-    return url.pathname.split('/')[1]
+    return url.pathname.split('/')[1];
   }
 
-  return null
+  return null;
 }
 
-function detectDailymotion (url, uri) {
+function detectDailymotion(url, uri) {
   if (uri.host.indexOf('dailymotion.com') > -1) {
-    return url.pathname.split('/')[2].split('_')[0]
+    return url.pathname.split('/')[2].split('_')[0];
   }
 
   if (uri.host === 'dai.ly') {
-    return url.pathname.split('/')[1]
+    return url.pathname.split('/')[1];
   }
 
-  return null
+  return null;
 }
 
-embed.vimeo = function (id, opts) {
-  opts = parseOptions(opts)
-  return '<iframe src="//player.vimeo.com/video/' + id + opts.query + '"' + opts.attr + ' frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>'
-}
+embed.vimeo = function(id, opts) {
+  opts = parseOptions(opts);
+  return `<iframe src="//player.vimeo.com/video/${id}${opts.query}"${opts.attr} frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>`;
+};
 
-embed.youtube = function (id, opts) {
-  opts = parseOptions(opts)
-  return '<iframe src="//www.youtube.com/embed/' + id + opts.query + '"' + opts.attr + ' frameborder="0" allowfullscreen></iframe>'
-}
+embed.youtube = function(id, opts) {
+  opts = parseOptions(opts);
+  return `<iframe src="//www.youtube.com/embed/${id}${opts.query}"${opts.attr} frameborder="0" allowfullscreen></iframe>`;
+};
 
-embed.dailymotion = function (id, opts) {
-  opts = parseOptions(opts)
-  return '<iframe src="//www.dailymotion.com/embed/video/' + id + opts.query + '"' + opts.attr + ' frameborder="0" allowfullscreen></iframe>'
-}
+embed.dailymotion = function(id, opts) {
+  opts = parseOptions(opts);
+  return `<iframe src="//www.dailymotion.com/embed/video/${id}${opts.query}"${opts.attr} frameborder="0" allowfullscreen></iframe>`;
+};
 
-embed.youtube.image = function (id, opts, cb) {
+embed.youtube.image = function(id, opts, cb) {
   if (typeof opts === 'function') {
-    cb = opts
-    opts = {}
+    cb = opts;
+    opts = {};
   }
-  opts = opts || {}
+  opts = opts || {};
 
-  opts.image = validYouTubeOpts.indexOf(opts.image) > 0 ? opts.image : 'default'
+  opts.image = validYouTubeOpts.indexOf(opts.image) > 0 ? opts.image : 'default';
 
-  var src = '//img.youtube.com/vi/' + id + '/' + opts.image + '.jpg'
+  const src = `//img.youtube.com/vi/${id}/${opts.image}.jpg`;
 
-  var result = {
-    src: src,
-    html: '<img src="' + src + '"/>'
-  }
+  const result = {
+    src,
+    html: `<img src="${src}"/>`,
+  };
 
-  if (!cb) return result.html
+  if (!cb) return result.html;
 
-  setTimeout(function () { cb(null, result) })
-}
+  setTimeout(() => {
+    cb(null, result);
+  });
+};
 
-embed.vimeo.image = function (id, opts, cb) {
+embed.vimeo.image = function(id, opts, cb) {
   if (typeof opts === 'function') {
-    cb = opts
-    opts = {}
+    cb = opts;
+    opts = {};
   }
 
-  if (!cb) throw new Error('must pass embed.vimeo.image a callback')
+  if (!cb) throw new Error('must pass embed.vimeo.image a callback');
 
-  opts = opts || {}
+  opts = opts || {};
 
-  opts.image = validVimeoOpts.indexOf(opts.image) >= 0 ? opts.image : 'thumbnail_large'
+  opts.image = validVimeoOpts.indexOf(opts.image) >= 0 ? opts.image : 'thumbnail_large';
 
-  fetch('https://vimeo.com/api/v2/video/' + id + '.json')
-    .then(function (res) {
-      if (res.status !== 200) {
-        throw new Error('unexpected response from vimeo')
-      }
+  fetch(`https://vimeo.com/api/v2/video/${id}.json`)
+      .then((res) => {
+        if (res.status !== 200) {
+          throw new Error('unexpected response from vimeo');
+        }
 
-      return res.json()
-    })
-    .then(function (body) {
-      if (!body || !body[0] || !body[0][opts.image]) {
-        throw new Error('no image found for vimeo.com/' + id)
-      }
+        return res.json();
+      })
+      .then((body) => {
+        if (!body || !body[0] || !body[0][opts.image]) {
+          throw new Error(`no image found for vimeo.com/${id}`);
+        }
 
-      var src = body[0][opts.image].split(':')[1]
+        const src = body[0][opts.image].split(':')[1];
 
-      var result = {
-        src: src,
-        html: '<img src="' + src + '"/>'
-      }
+        const result = {
+          src,
+          html: `<img src="${src}"/>`,
+        };
 
-      cb(null, result)
-    })
-    .catch(function (err) {
-      cb(err)
-    })
-}
+        cb(null, result);
+      })
+      .catch((err) => {
+        cb(err);
+      });
+};
 
-embed.dailymotion.image = function (id, opts, cb) {
+embed.dailymotion.image = function(id, opts, cb) {
   if (typeof opts === 'function') {
-    cb = opts
-    opts = {}
+    cb = opts;
+    opts = {};
   }
 
-  if (!cb) throw new Error('must pass embed.dailymotion.image a callback')
+  if (!cb) throw new Error('must pass embed.dailymotion.image a callback');
 
-  opts = opts || {}
+  opts = opts || {};
 
-  opts.image = validDailyMotionOpts.indexOf(opts.image) >= 0 ? opts.image : 'thumbnail_480_url'
+  opts.image = validDailyMotionOpts.indexOf(opts.image) >= 0 ? opts.image : 'thumbnail_480_url';
 
-  fetch('https://api.dailymotion.com/video/' + id + '?fields=' + opts.image)
-    .then(function (res) {
-      if (res.status !== 200) {
-        throw new Error('unexpected response from dailymotion')
-      }
+  fetch(`https://api.dailymotion.com/video/${id}?fields=${opts.image}`)
+      .then((res) => {
+        if (res.status !== 200) {
+          throw new Error('unexpected response from dailymotion');
+        }
 
-      return res.json()
-    })
-    .then(function (body) {
-      if (!body || !body[opts.image]) {
-        throw new Error('no image found for dailymotion.com/' + id)
-      }
+        return res.json();
+      })
+      .then((body) => {
+        if (!body || !body[opts.image]) {
+          throw new Error(`no image found for dailymotion.com/${id}`);
+        }
 
-      var src = body[opts.image]
+        const src = body[opts.image];
 
-      var result = {
-        src: src,
-        html: '<img src="' + src + '"/>'
-      }
+        const result = {
+          src,
+          html: `<img src="${src}"/>`,
+        };
 
-      cb(null, result)
-    })
-    .catch(function (err) {
-      cb(err)
-    })
+        cb(null, result);
+      })
+      .catch((err) => {
+        cb(err);
+      });
+};
+
+function serializeQuery(query) {
+  return Object.keys(query).map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(query[key])}`).join('&');
 }
 
-function serializeQuery (query) {
-  return Object.keys(query).map(function (key) {
-    return encodeURIComponent(key) + '=' + encodeURIComponent(query[key])
-  }).join('&')
-}
-
-function parseOptions (opts) {
-  var queryString = ''
-  var attributes = ''
+function parseOptions(opts) {
+  let queryString = '';
+  let attributes = '';
 
   if (opts && opts.hasOwnProperty('query')) {
-    queryString = '?' + serializeQuery(opts.query)
+    queryString = `?${serializeQuery(opts.query)}`;
   }
 
   if (opts && opts.hasOwnProperty('attr')) {
-    attributes = ' ' + Object.keys(opts.attr).map(function (key) {
-      return key + '="' + escape(opts.attr[key]) + '"'
-    }).join(' ')
+    attributes = ` ${Object.keys(opts.attr).map((key) => `${key}="${escape(opts.attr[key])}"`).join(' ')}`;
   }
 
-  return { query: queryString, attr: attributes }
+  return {query: queryString, attr: attributes};
 }
 
-module.exports = embed
+module.exports = embed;
